f950f2973cfa62c6d853c8c63343f4ee2218ea65
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2024-10-18 18:19:00 +0100

    chore:UI_changes

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -41,16 +41,16 @@ const HeroSection = () => {
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         {/* Blue background */}
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10 
-                        rounded-t-full bg-[#6ab0b5] w-1/2 aspect-[4/3]">
+                        rounded-t-full bg-[#6ab0b5] w-[40%] aspect-[4/3]">
         </div>
 
         {/* Hero title */}
-        <p className="xl:text-[28rem] lg:text-[15rem] delirium font-medium tracking-[2rem] text-center z-20 mt-[-15%] relative">
+        <p className="xl:text-[22rem] lg:text-[15rem] delirium font-medium tracking-[1.5rem] text-center z-20 mt-[-10%] relative">
           KELVIN DUCHE
         </p>
 
         {/* Tagline */}
-        <div className="absolute top-4 right-16 border border-white rounded-full px-4 py-2 text-sm z-50">
+        <div className="absolute top-32 right-[15%] border border-white rounded-full px-4 py-2 text-sm z-50 cursor-pointer">
           DESIGNING THE FUTURE
         </div>
 
@@ -62,7 +62,7 @@ const HeroSection = () => {
             width={800}
             height={800}
             alt="Kelvin Duche"
-            className="w-full h-auto relative"
+            className="w-[80%] h-auto mx-auto relative"
           />
         </div>
 
@@ -83,28 +83,28 @@ const HeroSection = () => {
         {/* Software icons */}
         <Image
           src="/images/figma.png"
-          className="absolute top-[20%] left-[5%] z-40 rotate-[-15deg] shake-on-hover cursor-pointer spin-animation"
+          className="absolute top-[20%] left-[15%] z-40 rotate-[-15deg] shake-on-hover cursor-pointer spin-animation"
           width={100}
           height={60}
           alt="Figma"
         />
         <Image
           src="/images/photoshop.svg"
-          className="absolute top-[30%] right-[10%] z-40 rotate-[15deg] shake-on-hover cursor-pointer spin-animation"
+          className="absolute top-[30%] right-[30%] z-40 rotate-[15deg] shake-on-hover cursor-pointer spin-animation"
           width={100}
           height={60}
           alt="Photoshop"
         />
         <Image
           src="/images/illustrator.svg"
-          className="absolute bottom-[50%] left-[20%] z-40 rotate-[15deg] shake-on-hover cursor-pointer spin-animation"
+          className="absolute bottom-[50%] left-[35%] z-40 rotate-[15deg] shake-on-hover cursor-pointer spin-animation"
           width={100}
           height={60}
           alt="Illustrator"
         />
 
         {/* Skill tags */}
-        <div className="absolute bottom-0 left-[5%] grid gap-2 z-0 py-6">
+        <div className="absolute bottom-0 left-[10%] grid gap-2 z-0 py-6">
           {/* First row with 3 columns */}
           <div className="grid grid-cols-3 items-center gap-2">
             <span className="bg-yellow-400 text-black px-3 py-3 rounded-full text-xl tracking-widest text-center">Visual Designer</span>
